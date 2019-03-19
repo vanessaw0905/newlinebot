d6f4193297b336001adbcab9d511989f7ec23562
vanessaw0905 <48682078+vanessaw0905@users.noreply.github.com>
2019-03-19 13:03:55 +0700

    Update profile.test.js

--- test/profile.test.js
+++ test/profile.test.js
@@ -17,9 +17,10 @@ nock(line).get(`/profile/${userId}`).reply(200, {
 });
 
 const bot = linebot({
-  channelId: 1234567890,
-  channelSecret: 'secret',
-  channelAccessToken: 'token'
+  channelId: 1556385294,
+  channelSecret: '1aeda682a0dc4df5bdd6ed3b9aaa0c7a',
+  channelAccessToken: 'r4xJ5eqfaef5gF61gi8CEDZgIm8B3vMLz8A+g9GSL/OsfYgNSAwhaZlBjnOWSnxH496MnFrQmKHIYl72kxMBNYxh7G6ZNi+6DW9vKSAo5TvycmNeN+zXlskHRz35nuFHj6VENiq5VO0/KCBCtvht5wdB04t89/1O/w1cDnyilFU=
+'
 });
 
 describe('Profile', function() {
